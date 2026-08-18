10731c3a4d055bbc5642607eecd3aefa329f5664
JuanGarcia <jmgc.9601@gmail.com>
2026-08-17 19:59:33 -0500

    Polishing details of the APP to improve structure and accessibility.

--- ASSETS_PALETA/JavaScript/APP_PALETA.js
+++ ASSETS_PALETA/JavaScript/APP_PALETA.js
@@ -2,6 +2,7 @@ const selector = document.getElementById("selector");
 const boton = document.querySelector("button");
 const caracteres = "0123456789ABCDEF";
 const contenedor = document.querySelector(".container-paleta");
+const feedback = document.querySelector(".microfeedback");
 
 selector.addEventListener("change", () => {
     const valor = selector.value;
@@ -11,6 +12,8 @@ selector.addEventListener("change", () => {
 boton.addEventListener("click" , () => {
     const valor = selector.value;
     const cantidad = parseInt(valor);
+    contenedor.classList.remove("grid-6", "grid-8", "grid-9");
+    contenedor.classList.add(`grid-${cantidad}`);
     contenedor.textContent = "";
     console.log(valor)
     console.log("Hiciste click")
@@ -24,6 +27,7 @@ boton.addEventListener("click" , () => {
                       console.log(color);
 
         const tarjeta = document.createElement("div");
+         tarjeta.className = "tarjeta";
         const zonaColor = document.createElement("div");
         const textoColor = document.createElement("p");   
 
@@ -36,4 +40,14 @@ boton.addEventListener("click" , () => {
         tarjeta.appendChild(textoColor);
         contenedor.appendChild(tarjeta);
     }
+        feedback.textContent = "¡Paleta generada!";
+        feedback.classList.add("visible");
+
+        setTimeout(() => {
+         feedback.classList.remove("visible");     
+          setTimeout(() => {
+        feedback.textContent = "";
+    }, 500);
+        }, 2000);  
     });
+
